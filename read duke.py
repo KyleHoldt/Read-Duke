f14662f9c1292a9f70844339fc3485554eac1197
KyleHoldt <kpholdt@gmail.com>
2017-06-28 07:07:31 -0500

    Add files via upload

--- read duke.py
+++ read duke.py
@@ -3,9 +3,23 @@ import urllib.request
 
 source = urllib.request.urlopen('http://duke.fm/music/trackhistory/?page=1').read()
 soup = bs.BeautifulSoup(source,'lxml')
+'''
+table = soup.table
+# or table = soup.find('table')
+table_rows = table.find_all('tr')
+for tr in table_rows:
+    rd = tr.find_all('td')
+    row = [i.text for i in td]
+    print(row)
+'''
+'''
+divs = soup.find_all('div')
 
-for div in soup.find_all('div', class_='body'):
-    print(div.text)
+for tr in divs.find('span'):
+    print(tr.text)
+'''
 
+spans = soup.div.find_all('span')
 
-
+for span in spans:
+    print(span.text)
