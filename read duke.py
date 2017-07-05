662decc6ce9b16150088ac17e78c83e9cc0c81bd
Kyle Holdt <KyleHoldt@users.noreply.github.com>
2017-07-05 16:49:18 -0500

    Add files via upload

--- read duke.py
+++ read duke.py
@@ -1,25 +1,34 @@
 import bs4 as bs
 import urllib.request
-
+import numpy as np
 source = urllib.request.urlopen('http://duke.fm/music/trackhistory/?page=1').read()
-soup = bs.BeautifulSoup(source,'lxml')
-'''
-table = soup.table
-# or table = soup.find('table')
-table_rows = table.find_all('tr')
-for tr in table_rows:
-    rd = tr.find_all('td')
-    row = [i.text for i in td]
-    print(row)
-'''
-'''
-divs = soup.find_all('div')
+soup = bs.BeautifulSoup(source,'html.parser')
 
-for tr in divs.find('span'):
-    print(tr.text)
+title = soup.find_all('span',{'class':'gmod-list-title'})
+artist = soup.find_all('span',{'class':'gmod-list-metadata'})
+time = soup.find_all('span',{'class':'gmod-list-time'})
+
+titles = []
+artists = []
+times = []
+def enlist(name, listname):
+    for i in name:
+        listname.append(i.string)
+enlist(title,titles)
+enlist(artist,artists)
+enlist(time,times)
 '''
+List = open('play_info.txt','a')
+for i in range(len(titles)):
+    ii = times[i] + '\t' + titles[i] + '\t' + artists[i].replace('by ','') + '\n'
+    List.write(ii)    
+List.close()
 
-spans = soup.div.find_all('span')
-
-for span in spans:
-    print(span.text)
+'''
+List = open('test.txt','a')
+data = []
+for i in range(len(titles)):
+    row[i] = (times[i],titles[i],artists[i])
+    data.append(row[i])
+List.write(data)    
+List.close()
