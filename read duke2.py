154f5257b648ef0f0d3efe9dcca051f3b5601439
Kyle Holdt <KyleHoldt@users.noreply.github.com>
2017-07-10 23:42:33 +0000

    Add files via upload

--- read duke2.py
+++ read duke2.py
@@ -1,13 +1,6 @@
 import bs4 as bs
 import urllib.request
-import numpy as np
-
-source = urllib.request.urlopen('http://duke.fm/music/trackhistory/?page=1').read()
-soup = bs.BeautifulSoup(source,'html.parser')
-
-title = soup.find_all('span',{'class':'gmod-list-title'})
-artist = soup.find_all('span',{'class':'gmod-list-metadata'})
-time = soup.find_all('span',{'class':'gmod-list-time'})
+from time import sleep
 
 prev_t = []
 titles = {}
@@ -17,9 +10,14 @@ def send_to_dict(element,Dict):
     if element not in Dict:
         Dict[element] = 1
     else:
-        Dict[element] = Dict[element] + 1
+        Dict[element] += 1
 
 while True:
+    source = urllib.request.urlopen('http://duke.fm/music/trackhistory/?page=1').read()
+    soup = bs.BeautifulSoup(source,'html.parser')
+    title = soup.find_all('span',{'class':'gmod-list-title'})
+    artist = soup.find_all('span',{'class':'gmod-list-metadata'})
+    time = soup.find_all('span',{'class':'gmod-list-time'})
     for i in range(len(title)):
         title_s = title[i].string
         artist_s = '!' + artist[i].string
@@ -30,4 +28,17 @@ while True:
     for titl in title:
         prev_t.append(titl.string)
     
-
+    titlefile = open('Titles.txt','w')
+    for song in titles:
+        line = '%s : %s\n'%(str(song),str(titles[song]))
+        titlefile.write(line)
+    titlefile.close()
+    artistfile = open('Artists.txt','w')
+    for singer in artists:
+        line = '%s : %s\n'%(str(singer),str(artists[singer]))
+        artistfile.write(line)
+    artistfile.close()
+    print(len(titles))
+    print(titles)
+    sleep( 900 )
+    
